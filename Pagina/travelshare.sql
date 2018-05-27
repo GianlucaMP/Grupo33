129b05ff280f12a11e1f5ece4e8a8b44e49b6f9e
GianlucaMP <37847642+GianlucaMP@users.noreply.github.com>
2018-05-27 01:19:47 -0300

    bbdd
    se añade la tabala de usuarios

--- Pagina/travelshare.sql
+++ Pagina/travelshare.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 15-05-2018 a las 02:21:00
+-- Tiempo de generación: 27-05-2018 a las 06:18:41
 -- Versión del servidor: 10.1.31-MariaDB
 -- Versión de PHP: 7.2.4
 
@@ -25,6 +25,28 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL,
+  `nombreusuario` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `fecha` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `nombreusuario`, `email`, `password`, `nombre`, `fecha`) VALUES
+(1, 'NOSEEE', 'q@q.com', '5e599803f8a7adba2f75be560ebc82f4', 'ELBA LAZO', '1990-05-02');
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `viajes`
 --
 
@@ -33,20 +55,51 @@ CREATE TABLE `viajes` (
   `preciototal` double NOT NULL,
   `origen` varchar(100) NOT NULL,
   `destino` varchar(100) NOT NULL,
-  `fecha` datetime NOT NULL,
+  `fecha` date NOT NULL,
   `vehiculo` varchar(100) NOT NULL,
   `contacto` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `viajes`
+--
+
+INSERT INTO `viajes` (`id`, `preciototal`, `origen`, `destino`, `fecha`, `vehiculo`, `contacto`) VALUES
+(1, 120, 'La Plata', 'Tandil', '2018-05-30', 'Toyota Corolla', 'Sr Pedro\r\nNro: 221 3445677'),
+(2, 350, 'Abasto', 'Centro', '2018-07-22', 'Honda 110 Biz', 'Sr John\r\n221 2334678'),
+(3, 555, 'CABA', 'Cordoba', '2018-06-02', 'Peugeot 206', 'Sr Titor\r\n011 32412398');
 
 --
 -- Índices para tablas volcadas
 --
 
 --
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indices de la tabla `viajes`
 --
 ALTER TABLE `viajes`
   ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `viajes`
+--
+ALTER TABLE `viajes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
